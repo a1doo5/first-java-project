7d00c195b5d1f6f2aa2471f7a17014968cc1f46d
Aidos Sarsenbay <aidossarsenbai0405@gmail.com>
2026-09-09 15:36:35 +0500

    Create first Java practice

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Java!");
-        System.out.println("JDK is ready!");
+        String product = "Coffee";
+        int quantity = 3;
+        double price = 1250.0;
+        double discountPercent = 10.0;
+
+        double totalPrice = quantity * price;
+        double discount = totalPrice * discountPercent / 100;
+        double finalPrice = totalPrice - discount;
+
+        System.out.println("Product: " + product);
+        System.out.println("Quantity: " + quantity);
+        System.out.println("Price without discount: " + totalPrice);
+        System.out.println("Discount: " + discount);
+        System.out.println("Final price: " + finalPrice);
     }
 }
